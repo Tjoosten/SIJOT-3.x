d6b6645fd5dba88468fa08fb86b6dcf80acb4b38
Tim Joosten <Tjoosten3@gmail.com>
2017-02-16 10:39:15 +0100

    Complete account security settings view.
    Signed-off-by: Tim Joosten <Tjoosten3@gmail.com>

--- resources/views/account/partials/account-security.blade.php
+++ resources/views/account/partials/account-security.blade.php
@@ -17,6 +17,22 @@
             <label for="confirmation" class="control-label col-sm-2">
                 Bevestiging wachtwoord: <span class="text-danger">*</span>
             </label>
+
+            <div class="col-sm-5">
+                <input id="confirmation" class="form-control" name="password_confirmation" placeholder="wachtwoord bevetiging" type="password">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-5">
+                <button type="submit" class="btn btn-flat btn-sm btn-success">
+                    <span class="fa fa-check" aria-hidden="true"></span> Wijzigen
+                </button>
+
+                <button type="reset" class="btn btn-flat btn-sm btn-danger">
+                    <span class="fa fa-undo" aria-hidden="true"></span> Reset formulier
+                </button>
+            </div>
         </div>
 
     </form>
